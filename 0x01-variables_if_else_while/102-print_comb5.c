9dd31eafa1d0525199a78e1707e51e4ec474962e
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-08-11 09:08:17 -0700

    final version

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,18 +13,12 @@ int main(void)
 	for (first = 0; first <= 98; first++)
 		for (second = first + 1; second <= 99; second++)
 		{
-			if (first < 10)
-				putchar('0');
-			else
-				putchar(first / 10 + '0');
+			putchar(first / 10 + '0');
 			putchar(first % 10 + '0');
 
 			putchar(' ');
 
-			if (second < 10)
-				putchar('0');
-			else
-				putchar(second / 10 + '0');
+			putchar(second / 10 + '0');
 			putchar(second % 10 + '0');
 
 			if (first != 98)
