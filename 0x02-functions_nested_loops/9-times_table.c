5f4e0e3d0774f8758e53d79edddee8784a4ea431
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-08-16 11:34:48 -0700

    100.0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -36,7 +36,7 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(ans / 100 + '0');
-				_putchar(ans / 10 + '0');
+				_putchar((ans / 10) % 10 + '0');
 				_putchar(ans % 10 + '0');
 			}
 		}
