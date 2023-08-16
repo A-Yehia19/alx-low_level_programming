68d4ed75634d9aa0d8cb72ef5fecdf729bb0a3ea
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-08-16 11:51:44 -0700

    102.0

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long long num[100], i;
+
+	num[0] = 1;
+	num[1] = 1;
+
+	printf("1, ");
+	for (i = 2 ; i < 51 ; i++)
+	{
+		num[i] = num[i-1] + num[i-2];
+		printf("%lld", num[i]);
+		if (i != 50)
+			printf(", ");
+	}
+
+	printf("\n");
+	return (0);
+}
