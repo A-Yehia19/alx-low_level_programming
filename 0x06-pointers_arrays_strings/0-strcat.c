57fd83f47d09d75636e6c63f0c9a622e770c7c66
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-08-23 16:08:29 -0700

    7.1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,8 @@ char *_strcat(char *dest, char *src)
 {
 	int len_dest, len_src;
 
-	for (len_dest = 0 ; dest[len_dest] != '\0' ; len_dest++);
+	for (len_dest = 0 ; dest[len_dest] != '\0' ; len_dest++)
+		;
 
 	for (len_src = 0 ; src[len_src] != '\0' ; len_src++, len_dest++)
 		dest[len_dest] = src[len_src];
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,8 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int len_dest, len_src;
 
-	for (len_dest = 0 ; dest[len_dest] != '\0' ; len_dest++);
+	for (len_dest = 0 ; dest[len_dest] != '\0' ; len_dest++)
+		;
 
 	for (len_src = 0 ; src[len_src] != '\0' && n; len_src++, len_dest++, n--)
 		dest[len_dest] = src[len_src];
