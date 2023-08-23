f845695a4d2c262eea843640afee5f47b77b1c33
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-08-24 02:11:34 +0300

    7.2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,8 +16,8 @@ char *cap_string(char *str)
 			str[i] == '{' || str[i] == '}' || str[i] == '(' ||
 			str[i] == ')' || str[i] == ';' || str[i] == '"' ||
 			str[i] == '!' || str[i] == '?' || str[i] == '.' || str[i] == '\t')
-			if(str[i+1] >= 'a' && str[i+1] <= 'z')
-				str[i+1] = str[i+1] - 'a' + 'A';
+			if(str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] = str[i + 1] - 'a' + 'A';
 		if(i == 0)
 			if(str[i] >= 'a' && str[i] <= 'z')
 				str[i] = str[i] - 'a' + 'A';
