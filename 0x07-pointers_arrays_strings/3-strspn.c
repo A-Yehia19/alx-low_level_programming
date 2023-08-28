16d2224e040ae47b26cd6674560e050efa32af1b
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-08-28 15:05:40 +0300

    4.0

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,22 +12,23 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int end, i;
 
-    for (end = 0 ; s[end] != '\0'; end++){
-        int found;
+	for (end = 0 ; s[end] != '\0'; end++)
+	{
+		int found;
 
-        found = 0;
-        for (i = 0; accept[i] != '\0'; i++)
-        {
-            if (accept[i] == s[end])
-            {
-                found = 1;
-                break;
-            }
-        }
+		found = 0;
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (accept[i] == s[end])
+			{
+				found = 1;
+				break;
+			}
+		}
 
-        if (!found)
-        break;
-    }
+		if (!found)
+			break;
+	}
 
-    return (end);
+	return (end);
 }
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,5 +9,25 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	
+	int i,found;
+
+	while (*s != '\0')
+	{
+		found = 0;
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (accept[i] == *s)
+			{
+				found = 1;
+				break;
+			}
+		}
+
+		if (found)
+			break;
+
+		s++;
+	}
+
+	return (s);
 }
