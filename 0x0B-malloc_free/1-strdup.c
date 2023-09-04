924ef8cbbbdd6f38429ca8850b972cc42ccca7ed
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-04 13:32:17 +0300

    1.0

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _strdup - function name
@@ -8,5 +9,19 @@
 
 char *_strdup(char *str)
 {
-	
+	char *p;
+	unsigned int i, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	p = malloc(len * sizeof(char));
+	for (i = 0; i < len; i++)
+		p[i] = str[i];
+	p[len] = '\0';
+
+	return (p);
 }
