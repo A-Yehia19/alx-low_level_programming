7beeb201dbf512877ccead0e44fabe516e824098
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-04 15:23:35 +0300

    advanced

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,5 +10,36 @@
 
 char *argstostr(int ac, char **av)
 {
-	
+	char *res;
+	int i, tmp, len, pos;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	len = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (tmp = 0; av[i][tmp] != '\0'; tmp++)
+			;
+		len += tmp + 1;
+	}
+
+	res = malloc((len + 1) * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+
+	pos = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (tmp = 0; av[i][tmp] != '\0'; tmp++)
+		{
+			res[pos] = av[i][tmp];
+			pos++;
+		}
+		res[pos] = '\n';
+		pos++;
+	}
+	res[pos] = '\0';
+
+	return (res);
 }
