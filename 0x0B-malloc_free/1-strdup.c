c2160a660c6f29dcf5ba463b2e436c04dc99df9d
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-04 13:34:40 +0300

    1.1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,10 +18,12 @@ char *_strdup(char *str)
 	for (len = 0; str[len] != '\0'; len++)
 		;
 
-	p = malloc(len * sizeof(char));
-	for (i = 0; i < len; i++)
+	p = malloc((len - 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
 		p[i] = str[i];
-	p[len] = '\0';
 
 	return (p);
 }
