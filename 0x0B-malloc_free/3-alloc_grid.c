f571b3824907a4c33efb74c7cad806257340fb37
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-04 11:18:45 +0300

    3.0

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,5 +10,25 @@
 
 int **alloc_grid(int width, int height)
 {
-	
+	int **p;
+	int row, col;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	p = (int **) malloc(height * sizeof(int *));
+	if (p == NULL)
+		return(NULL);
+
+	for (row = 0; row < height; row++)
+	{
+		p[row] = (int *) malloc(width * sizeof(int));
+		if (p[row] == NULL)
+			return(NULL);
+
+		for (col = 0; col < width; col++)
+			p[row][col] = 0;
+	}
+
+	return(p);
 }
