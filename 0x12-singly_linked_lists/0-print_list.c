5731f8b4d3905555f2ff99b1962e4723012fad25
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-21 15:28:49 +0300

    betty fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,17 +11,17 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t len;
+	size_t len;
 
 	for (len = 0; h != NULL; len++)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-        h = h->next;
-    }
+		h = h->next;
+	}
 
-    return (len);
+	return (len);
 }
