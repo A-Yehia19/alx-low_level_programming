fc9ba15564bebdb2e6fba1bb3076e30af69347b5
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-21 16:04:50 +0300

    advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * printBefore - function name
+ * @void: input
+ * Return: result
+ */
+
+void printBefore(void) __attribute__ ((constructor));
+
+void printBefore(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
