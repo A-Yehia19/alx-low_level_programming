1451f3c4fb2a6e55a39809b2d9276a6554114b08
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-21 16:08:16 +0300

    betty fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,10 +7,10 @@
  * Return: result
  */
 
-void printBefore(void) __attribute__ ((constructor));
+void printBefore(void)__attribute__ ((constructor));
 
 void printBefore(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
