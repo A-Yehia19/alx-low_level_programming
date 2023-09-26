16ad6ed3bb0af34c04f51a35bd68a6263e598e9f
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-27 00:30:53 +0300

    fixing errors

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -17,7 +17,7 @@ void free_listint2(listint_t **head)
 	if (head == NULL || *head == NULL)
 		return;
 
-	for (pointer = *head; pointer->next != NULL; pointer = next)
+	for (pointer = *head; pointer != NULL; pointer = next)
 	{
 		next = pointer->next;
 		free(pointer);
