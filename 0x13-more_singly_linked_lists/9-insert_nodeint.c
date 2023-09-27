a1852fd209a4f73e2d8165dd1322fc80cbf1f195
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-27 17:17:30 +0300

    testing case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL || *head == NULL)
 		return (NULL);
 
-	for (counter = 0; counter < idx - 1 && pointer->next && pointer; counter++)
+	for (counter = 0; counter < idx - 1 && pointer->next != NULL; counter++)
 	{
 		pointer = pointer->next;
 	}
