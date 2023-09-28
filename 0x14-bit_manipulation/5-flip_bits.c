7b0927d592983bd6c5171546f072cfaf1f2ba2e4
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-28 16:35:17 +0300

    first try

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,5 +10,17 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	
+	unsigned int ans;
+
+	ans = 0;
+	while (n != m)
+	{
+		if ((n & 1) != (m & 1))
+			ans++;
+
+		n >>= 1;
+		m >>= 1;
+	}
+
+	return ans;
 }
