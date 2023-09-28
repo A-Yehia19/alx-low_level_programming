32d677acb6bfa11d3d982e23450a82660d180fcc
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-28 16:26:39 +0300

    first try

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,5 +10,12 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	
+	unsigned long int mask;
+
+	if (index > 63)
+		return (-1);
+
+	mask = 1 << index;
+	*n = *n | mask;
+	return (1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,5 +10,12 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	
+	unsigned long int mask;
+
+	if (index > 63)
+		return (-1);
+
+	mask = -2 << index;
+	*n = *n & mask;
+	return (1);
 }
