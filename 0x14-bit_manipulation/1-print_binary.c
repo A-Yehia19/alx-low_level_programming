956ed425dc2cdc81db3b0d7015ac569b3b1aa940
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-28 16:09:22 +0300

    betty fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,27 +9,27 @@
 
 void print_binary(unsigned long int n)
 {
-    unsigned long int reversed, len;
+	unsigned long int reversed, len;
 
-    if (n == 0)
-    {
-        _putchar ('0');
-        return;
-    }
+	if (n == 0)
+	{
+		_putchar ('0');
+		return;
+	}
 
-    len = 0;
+	len = 0;
 	for (reversed = 0; n; n = n >> 1)
-    {
-        reversed = reversed << 1;
-        reversed = reversed | (n & 1);
-        len++;
-    }
+	{
+		reversed = reversed << 1;
+		reversed = reversed | (n & 1);
+		len++;
+	}
 
-    while (len)
-    {
-        _putchar((reversed & 1) + '0');
-        reversed = reversed >> 1;
-        len--;
-    }
-    
+	while (len)
+	{
+		_putchar((reversed & 1) + '0');
+		reversed = reversed >> 1;
+		len--;
+	}
+
 }
