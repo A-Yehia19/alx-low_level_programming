bd1c253f51631b40894dd335658dd46b620af182
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-28 15:51:32 +0300

    first try

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,4 +9,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int ans, i;
+
+	if (b == NULL)
+		return (0);
+
+	ans = 0;
+	for (i = 0; b[i] != '\0'; i++)
+		if (b[i] == '0' || b[i] == '1')
+		{
+			ans = ans << 1;
+			ans = ans | (b[i] - '0');
+		}
+		else
+			return (0);
+
+	return (ans);
 }
