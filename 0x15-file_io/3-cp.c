f3b4e671572bcaa0d3e07c46b318f0ad50bee6b1
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-10-03 15:55:46 +0300

    testing cases

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,52 +11,40 @@ int main(int ac, char **av)
 {
 	int file_from, file_to, size;
 	char *buffer;
-
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: %s filename text\n", av[0]);
 		exit(97);
 	}
-
 	file_from = open(av[0], O_RDONLY);
-	file_to = open(av[0], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND);
-
+	file_to = open(av[0], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
 	if (file_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
 	if (file_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-
-
-
-	/* wirting on file */
+	/* writing on file */
 	buffer = malloc(1024 * sizeof(char));
-
-	do
-	{
-		size = read (file_from, buffer, sizeof(buffer));
+	do {
+		size = read(file_from, buffer, sizeof(buffer));
 		write(file_to, buffer, size);
 	} while (size != 0);
-
-
-
 	/* closing file */
 	if (close(file_from) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", file_from);
-		exit(99);
+		exit(100);
 	}
 	if (close(file_to) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", file_to);
-		exit(99);
+		exit(100);
 	}
-
+	free(buffer);
 	return (0);
 }
