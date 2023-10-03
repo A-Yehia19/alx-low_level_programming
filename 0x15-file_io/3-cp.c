5c6a6e85f3f1c8ebf23bb372d99ff687fdc23959
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-10-03 13:17:42 +0300

    first try

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,5 +9,54 @@
  */
 int main(int ac, char **av)
 {
-    
+	int file_from, file_to, size;
+	char *buffer;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(97);
+	}
+
+	file_from = open(av[0], O_RDONLY);
+	file_to = open(av[0], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND);
+
+	if (file_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	if (file_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+
+
+	/* wirting on file */
+	buffer = malloc(1024 * sizeof(char));
+
+	do
+	{
+		size = read (file_from, buffer, sizeof(buffer));
+		write(file_to, buffer, size);
+	} while (size != 0);
+
+
+
+	/* closing file */
+	if (close(file_from) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", file_from);
+		exit(99);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", file_to);
+		exit(99);
+	}
+
+	return (0);
 }
--- 0x15-file_io/main.c
+++ 0x15-file_io/main.c
@@ -8,6 +8,7 @@ int main(int ac, char **av)
 
     if (ac != 3)
     {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
         exit(1);
     }
     res = create_file(av[1], av[2]);
