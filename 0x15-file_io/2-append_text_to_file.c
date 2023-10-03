1bdea6e643e0c429687035602811e076ff1afb7f
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-10-03 13:04:00 +0300

    first try

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,5 +9,23 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	
+	int file, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+
+	write(file, text_content, len);
+	close(file);
+	return (1);
 }
