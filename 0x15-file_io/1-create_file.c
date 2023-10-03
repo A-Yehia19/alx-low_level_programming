8e1c5abff4fdc82d67e904d7832352bbd22e496c
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-10-03 12:59:57 +0300

    first try

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,5 +9,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	
+	int file, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+
+	write(file, text_content, len);
+	close(file);
+	return (1);
+
 }
