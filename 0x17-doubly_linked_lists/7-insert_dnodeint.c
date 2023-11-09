35505a833ab6193c1f13f1d616c0343a626b7578
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-11-09 19:53:45 +0200

    testing 9

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,19 +10,16 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *next, *prev = *h, *node = malloc(sizeof(dlistint_t));
+	dlistint_t *next, *prev = *h;
+	dlistint_t *node = malloc(sizeof(dlistint_t));
 	if (!node)
 		return (NULL);
+
 	node->n = n;
 	node->next = NULL;
 	node->prev = NULL;
 
-	if (!*h && idx != 0)
-	{
-		free(node);
-		return (NULL);
-	}
-	else if (!*h && idx == 0)
+	if (!*h && idx == 0)
 		*h = node;
 	else if (idx == 0)
 	{
@@ -41,9 +38,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			}
 			prev = prev->next;
 		}
-		next = prev->next;
-		next->prev = node;
-		node->next = next;
+		if (prev->next)
+		{
+			next = prev->next;
+			next->prev = node;
+			node->next = next;
+		}
 		prev->next = node;
 		node->prev = prev;
 	}
