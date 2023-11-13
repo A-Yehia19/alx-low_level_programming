d7406a39907f444b45c3caf86d3f64dd4413797e
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-11-13 17:17:08 +0200

    project init

---
+++ 0x18-dynamic_libraries/isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - function name
+ * @c: input argument
+ * Return: result
+ */
+
+int _isdigit(int c)
+{
+	return ('0' <= c && c <= '9');
+}
---
+++ 0x18-dynamic_libraries/isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - task
+ * @c: input argument
+ * Return: result
+ */
+
+int _isupper(int c)
+{
+	return ('A' <= c && c <= 'Z');
+}
