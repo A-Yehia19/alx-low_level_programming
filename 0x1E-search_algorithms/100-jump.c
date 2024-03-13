3306decbc8db78e42c2072d7485416bca1a4371d
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-03-13 12:45:30 +0200

    test case

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,4 +1,3 @@
-#include "search_algos.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
@@ -14,19 +13,20 @@
 
 int jump_search(int *array, size_t size, int value)
 {
+	int answer;
 	size_t current, steps;
 
-	/* jumps */
+	answer = -1;
 	steps = sqrt(size);
-	current = 0;
-	do {
+
+	/* jumps */
+	for (current = 0; current < size && array != NULL; current += steps)
+	{
+		if (array[current] >= value)
+			break;
+
 		printf("Value checked array[%lu] = [%d]\n", current, array[current]);
-
-		if (array[current] == value)
-			return (current);
-
-		current += steps;
-	} while (current < size && array[current] < value);
+	}
 
 	/* linear search until found or end of array */
 	printf("Value found between indexes [%lu] and [%lu]\n",
@@ -38,8 +38,24 @@ int jump_search(int *array, size_t size, int value)
 				current, array[current]);
 
 		if (array[current] == value)
-			return (current);
+		{
+			answer = value;
+			break;
+		}
 	}
 
-	return (-1);
+	return (answer);
+}
+
+int main(void)
+{
+    int array[] = {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Found %d at index: %d\n\n", 6, jump_search(array, size, 6));
+    printf("Found %d at index: %d\n\n", 1, jump_search(array, size, 1));
+    printf("Found %d at index: %d\n", 999, jump_search(array, size, 999));
+    return (EXIT_SUCCESS);
 }
