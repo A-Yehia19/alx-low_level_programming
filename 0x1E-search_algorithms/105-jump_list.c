a74f39b2a4c0635d2ec0920f5cf482d3bc7aa34c
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-03-13 12:59:46 +0200

    advanced run

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,88 @@
+#include "search_algos.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * get_index - get the index of a linked list
+ * @list: linked list
+ * @index: index to get
+ * 
+ * Return: node at index
+ */
+
+listint_t *get_index(listint_t *list, size_t index)
+{
+	size_t i;
+
+	for (i = 0; i < index; i++)
+		list = list->next;
+
+	return (list);
+}
+
+/**
+ * jump_list - perform jump search on an linked list
+ * @array: array to serach in
+ * @size: size of linked list
+ * @value: value to search in
+ *
+ * Return: node of first occurence
+ */
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t current, steps;
+
+	if (list == NULL)
+		return (NULL);
+
+	/* jumps */
+	steps = sqrt(size);
+	current = 0;
+	do {
+		printf("Value checked array[%lu] = [%d]\n", current, get_index(list, current)->n);
+
+		if (get_index(list, current)->n == value)
+			return (get_index(list, current));
+
+		current += steps;
+	} while (current < size && get_index(list, current)->n < value);
+
+	/* linear search until found or end of array */
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			current - steps, current);
+
+	for (current -= steps; current < size ; current++)
+	{
+		printf("Value checked array[%lu] = [%d]\n",
+				current, get_index(list, current)->n);
+
+		if (get_index(list, current)->n == value)
+			return (get_index(list, current));
+	}
+
+	return (NULL);
+}
+
+int main(void)
+{
+    listint_t *list, *res;
+    int array[] = {
+        0, 1, 2, 3, 4, 7, 12, 15, 18, 19, 23, 53, 61, 62, 76, 99
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    list = create_list(array, size);
+    print_list(list);
+
+    res =  jump_list(list, size, 53);
+    printf("Found %d at index: %lu\n\n", 53, res->index);
+    res =  jump_list(list, size, 2);
+    printf("Found %d at index: %lu\n\n", 2, res->index);
+    res =  jump_list(list, size, 999);
+    printf("Found %d at index: %p\n", 999, (void *) res);
+
+    free_list(list);
+    return (EXIT_SUCCESS);
+}
